6e77e6035af6247fa39444fa949188eab1fd31fd
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-25 19:53:34 -0700

    Ejemplo 2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Ejemplo 1
-var car = {
+/* var car = {
     brand: 'Nissan',
     model: 'Sentra',
     year: '2021'
@@ -16,4 +16,28 @@ var sameCar = addColor(car);
 console.log(`car after calling addColor() ${JSON.stringify(car)}`);
 console.log(`sameCar after calling addColor() ${JSON.stringify(sameCar)}`)
 
-console.log(`Same car? ${car === sameCar}`);
+console.log(`Same car? ${car === sameCar}`); */
+
+//Ejemplo 2
+
+let cart = [
+    {
+        item: 'Laptop',
+        quantity: 1
+    }
+];
+
+function addItemToCart(cart, item, quantity) {
+    let newCart = cart.map(el => el);
+
+    newCart.push({
+        item: item,
+        quantity: quantity
+    })
+
+    return newCart;
+}
+
+cart = addItemToCart(cart, 'Phone', 2);
+
+console.log(`Cart ${JSON.stringify(cart)}`)
